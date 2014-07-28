5b5918120205fd549cb40a5eb783083522d26db7
Pillows <pillows@users.noreply.github.com>
2014-07-27 22:32:57 -0400

    Added a /r/ option for subreddits.

--- go.py
+++ go.py
@@ -3,8 +3,12 @@ import webbrowser
 
 def go(string):
     check = re.findall("go to http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+", string)
+    reddit = re.findall("go to /r/[a-z]*",string)
     if check:
         check[0] = check[0].replace("go to ", '')
         webbrowser.open(check[0])
-
+    else:
+        if reddit:
+            reddit[0] = reddit[0].replace("go to ", '')
+            webbrowser.open("http://reddit.com" + reddit[0])
     
