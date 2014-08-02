5770e4e9e38981258db4b72df7ced40128bc6e2f
Joe Ellis <jce.devel@gmail.com>
2014-08-02 17:35:28 +0100

    Made installable as a command line application.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import *
+
+kwargs = {
+	"author" : "Frankie Primerano",
+	"author_email" : "max00355@gmail.com",
+	"description" : "Hey is a terminal helper that allows you to execute commands using plain old English.",
+	"entry_points" : {"console_scripts" : ["hey=hey.hey:main"]},
+	"name" : "hey",
+	"packages" : ["hey"],
+}
+
+setup(**kwargs)
